936b7debb3ff49a40f180375025249d6605bc62c
Nurislam Zaripov <znurislama56@gmail.com>
2022-03-19 18:50:49 +0300

    Complete all classes.

--- src/dynamic_array.cpp
+++ src/dynamic_array.cpp
@@ -13,64 +13,126 @@ namespace assignment {
     }
 
     // Write your code here ...
+    size_ = 0;
+    capacity_ = capacity;
+    data_ = new int[capacity_]{0};
   }
 
   DynamicArray::~DynamicArray() {
     // Write your code here ...
+    capacity_ = 0;
+    delete[] data_;
+    data_ = nullptr;
+    size_ = 0;
   }
 
   void DynamicArray::Add(int value) {
     // Write your code here ...
+    if (size_ >= capacity_) {
+      Resize(size_ + kCapacityGrowthCoefficient);
+    }
+    data_[size_] = value;
+    size_++;
   }
 
   bool DynamicArray::Insert(int index, int value) {
     // Write your code here ...
-    return false;
+    if (index < 0 || index > size_ ) {
+      return false;
+    } else if (size_ >= capacity_) {
+      Resize(size_ + kCapacityGrowthCoefficient);
+    }
+
+    for (int i = size_; i > index; i--) {
+      data_[i] = data_[i-1];
+    }
+    data_[index] = value;
+    size_++;
+    return true;
   }
 
   bool DynamicArray::Set(int index, int new_value) {
     // Write your code here ...
-    return false;
+    if (index < 0 || index >= size_) {
+      return false;
+    }
+    data_[index] = new_value;
+    return true;
   }
 
   std::optional<int> DynamicArray::Remove(int index) {
     // Write your code here ...
+    if (index >= 0 && index < size_) {
+      int deletedInt = data_[index];
+      for (int i = index; i < size_; i++) {
+        data_[i] = data_[i+1];
+      }
+      size_--;
+      return deletedInt;
+    }
     return std::nullopt;
   }
 
   void DynamicArray::Clear() {
     // Write your code here ...
+    delete[] data_;
+    data_ = new int[kInitCapacity]{};
+    size_ = 0;
+
   }
 
   std::optional<int> DynamicArray::Get(int index) const {
     // Write your code here ...
+    if (index >= 0 && index < size_) {
+      return data_[index];
+    }
     return std::nullopt;
   }
 
   std::optional<int> DynamicArray::IndexOf(int value) const {
     // Write your code here ...
+    for (int i = 0; i < size_; i++) {
+      if (data_[i] == value) {
+        return i;
+      }
+    }
     return std::nullopt;
   }
 
   bool DynamicArray::Contains(int value) const {
-    return false;
+    return (this->IndexOf(value) == std::nullopt) ? false : true;
   }
 
   bool DynamicArray::IsEmpty() const {
-    return false;
+    return size_ == 0;
   }
 
   int DynamicArray::size() const {
-    return 0;
+    return size_;
   }
 
   int DynamicArray::capacity() const {
-    return 0;
+    return capacity_;
   }
 
   bool DynamicArray::Resize(int new_capacity) {
     // Write your code here ...
-    return false;
+    if (new_capacity <= capacity_) {
+      return false;
+    }
+    int* newData = new int[new_capacity];
+    for (int i = 0; i < size_; i++) {
+      newData[i] = data_[i];
+    }
+    for (int i = size_; i < new_capacity; i++) {
+      newData[i] = 0;
+    }
+
+    delete[] data_;
+    data_ = newData;
+    newData = nullptr;
+    capacity_ = new_capacity;
+    return true;
   }
 
   // ДЛЯ ТЕСТИРОВАНИЯ
--- src/linked_list.cpp
+++ src/linked_list.cpp
@@ -10,61 +10,178 @@ namespace assignment {
 
   void LinkedList::Add(int value) {
     // Write your code here ...
+    Node* node = new Node(value, nullptr);
+    if (size_ == 0) {
+      front_ = node;
+      back_ = node;
+    } else {
+      back_->next = node;
+      back_ = node;
+    }
+    size_++;
   }
 
   bool LinkedList::Insert(int index, int value) {
     // Write your code here ...
-    return false;
+    if (index < 0 || index > size_) {
+      return false;
+    }
+
+    Node* newNode = new Node(value, nullptr);
+    if (size_ == 0 || index == size_) {
+      Add(value);
+    } else {
+      if (index == 0) {
+        newNode->next = front_;
+        front_ = newNode;
+        size_++;
+      } else {
+
+        int i = 0;
+        Node* node = front_;
+        while (node != nullptr) {
+          if (i == index - 1) {
+            newNode->next = node->next;
+            node->next = newNode;
+            break;
+          }
+          i++;
+          node = node->next;
+        }
+        size_++;
+      }
+    }
+    return true;
   }
 
   bool LinkedList::Set(int index, int new_value) {
+    if (index < 0 || index >= size_) {
+      return false;
+    }
+
+    int i = 0;
+    for (Node* node = front_; node != nullptr; node = node->next) {
+      if (i == index) {
+        node->value = new_value;
+        return true;
+      }
+      i++;
+    }
     return false;
   }
 
   std::optional<int> LinkedList::Remove(int index) {
     // Write your code here ...
-    return std::nullopt;
+    if (index < 0 || index >= size_) {
+      return std::nullopt;
+    }
+
+    Node* forDeleteNode = nullptr;
+    int value;
+
+    if (index == 0) {
+      forDeleteNode = front_;
+      front_ = front_->next;
+      value = forDeleteNode->value;
+    } else {
+
+      int i = 0;
+      for (Node* node = front_; node != nullptr; node = node->next) {
+        if (i == index - 1) {
+          forDeleteNode = node->next;
+          node->next = forDeleteNode->next;
+          value = forDeleteNode->value;
+          break;
+        }
+        i++;
+      }
+    }
+    delete forDeleteNode;
+    size_--;
+    return value;
   }
 
   void LinkedList::Clear() {
     // Write your code here ...
+    if (size_ > 0) {
+      Node* node = front_;
+      Node* forDeleteNode = nullptr;
+      while(node != nullptr) {
+        forDeleteNode = node;
+        node = node->next;
+        delete forDeleteNode;
+      }
+    }
+    size_ = 0;
+    front_ = nullptr;
+    back_ = nullptr;
   }
 
   std::optional<int> LinkedList::Get(int index) const {
     // Write your code here ...
-    return std::nullopt;
+    if (index < 0 || index >= size_) {
+      return std::nullopt;
+    }
+    Node* listNode = front_;
+    for (int i = 1; i <= index; i++) {
+      listNode = listNode->next;
+    }
+    return listNode->value;
   }
 
   std::optional<int> LinkedList::IndexOf(int value) const {
     // Write your code here ...
+    Node* listNode = front_;
+    for (int i = 0; i < size_; i++) {
+      if (listNode->value == value) {
+        return i;
+      }
+      listNode = listNode->next;
+    }
     return std::nullopt;
   }
 
   bool LinkedList::Contains(int value) const {
+    if (this->IndexOf(value) != std::nullopt) {
+      return true;
+    }
     return false;
   }
 
   bool LinkedList::IsEmpty() const {
-    return false;
+    return size_ == 0;
   }
 
   int LinkedList::size() const {
-    return 0;
+    return size_;
   }
 
   std::optional<int> LinkedList::front() const {
     // Write your code here ...
+    if (front_ != nullptr) {
+      return front_->value;
+    }
     return std::nullopt;
   }
 
   std::optional<int> LinkedList::back() const {
     // Write your code here ...
+    if (back_ != nullptr) {
+      return back_->value;
+    }
     return std::nullopt;
   }
 
   Node* LinkedList::FindNode(int index) const {
     // Write your code here ...
-    return nullptr;
+    if (index < 0 || index >= size_) {
+      return nullptr;
+    }
+    Node* listNode = front_;
+    for (int i = 0; i < index; ++i) {
+      listNode = listNode->next;
+    }
+    return listNode;
   }
 
   // ДЛЯ ТЕСТИРОВАНИЯ
